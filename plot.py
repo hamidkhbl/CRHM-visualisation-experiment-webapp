745a8cd6f6a792d7c6c8f5b4713d26d1b911347f
Hamid Khodabandehloo <hamidkhbl@gmail.com>
2020-04-19 11:35:09 -0600

    Fix date

--- plot.py
+++ plot.py
@@ -12,6 +12,13 @@ obsFileName = sys.argv[1]
 if not os.path.exists('csv'):
     os.makedirs('csv')
 
+# convert time
+def fixTime(wtime):
+    wtime = wtime.replace(' ','-',2)
+    wtime = wtime.replace(' ',':',2)
+    wtime = wtime.replace(':',' ',1)
+    return wtime
+
 # covert obs to csv
 def convertToCsv(fileName):
     print(colored("Info",'green'),": converting obs to csv...")
@@ -42,8 +49,21 @@ obsColumnsTm = ['time'] + obsColumns
 
 df = pd.DataFrame([x.split('\t') for x in bodyList], columns = obsColumnsTm)
 
-df.to_csv('test.csv')
+try:
+    df['time'] = pd.to_datetime(df['time'])
+except:
+    print()
+    print(colored('Warning','yellow'),': Wrong fromat for time. Trying to convert...')
+    print()
+    df['time'] = df['time'].apply(lambda x : fixTime(x))
+    try:
+        df['time'] = pd.to_datetime(df['time'])
+        print(colored('Info','green'), ': Time is fixed')
+        print()
+    except:
+        print(colored('Info','red'),': Was not able to fix the time issue. Please make sure your time format is: YYYY-MM-DD HH:MM')
 
+#df.to_csv('test.csv')
 data = []
 for x in obsColumns:
     trace = go.Scatter(x=df['time'], 
@@ -51,8 +71,11 @@ for x in obsColumns:
                         mode='lines',
                         name=x)
     data.append(trace)
-print(colored('Info','green')," Generating plot for {}...".format(obsFileName))
-layout = go.Layout(title=obsFileName)
+print(colored('Info','green'),": Generating plot for {}...".format(obsFileName))
+layout = go.Layout(title='<b>'+ obsFileName +'<b>', titlefont=dict(family="Balto",
+                                                    size=35,
+                                                    color="black"
+                                                    ))
 fig = go.Figure(data=data, layout=layout)
 pyo.plot(fig)
 
