69447d67734e14043d1c8b528856fd07688368d5
Hamid Khodabandehloo <hamidkhbl@gmail.com>
2020-04-19 09:20:59 -0600

    Get headr of obs file

--- plot.py
+++ plot.py
@@ -19,10 +19,22 @@ def convertToCsv(fileName):
     df.to_csv('csv/{0}.csv'.format(fileName))
     print(colored("Info",'green'),": csv format of {0} saved to csv folder.".format(fileName))
 
-# read the obs file
+# read header of the obs file
+
+obsColumns = []
 with open(obsFileName,mode='r') as f:
-    content = f.read()
-print(content.index("SWE(1)"))
+    for x in range(100):
+        head = next(f)
+        obsColumns.append(head.strip())
+        if head.strip() == '######':
+            print(x-1)
+            break
+
+obsColumns = obsColumns[1:len(obsColumns)-1]
+obsColumns
+print(obsColumns)
+
+
 
 
 
